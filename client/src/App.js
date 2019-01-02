ed82284381cacbd921c9161b5c9e0d247079cb00
Jiaying Liu <jiaying_liu1996@hotmail.com>
2019-01-01 21:08:45 -0500

    enabled chatting ability

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import openSocket from 'socket.io-client';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.socket = openSocket('http://localhost:5000');
+    this.socket.on('chat message', msg => {
+      const messages = this.state.messages;
+      messages.push(msg);
+
+      this.setState({
+        messages,
+        msg: ''
+      });
+    });
+
+    this.state = {
+      msg: '',
+      messages: []
+    }
+  }
+
+  onMsgInputChange = e => {
+    this.setState({
+      msg: e.target.value
+    });
+  }
+
+  enterBind = e => {
+    const code = e.keyCode || e.which;
+    
+    if(code === 13) {
+      this.sendMsg();
+    }
+  }
+
+  sendMsg = () => {
+    this.socket.emit('chat message', this.state.msg);
+  }
+
+  renderMessages() {
+    return this.state.messages.map((message, index) => {
+      return (
+        <li key={index}>
+          {message}
+        </li>
+      )
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {this.renderMessages()}
+        <input
+          value={this.state.msg}
+          onChange={this.onMsgInputChange}
+          onKeyPress={this.enterBind} />
+        <button onClick={this.sendMsg}>
+          Enter
+        </button>
       </div>
     );
   }
--- server/index.js
+++ server/index.js
@@ -13,6 +13,6 @@ io.on('connection', client => {
     })
 });
 
-http.listen(5000, () => {
+http.listen(5000, function() {
     console.log('listening on 5000');
 });
